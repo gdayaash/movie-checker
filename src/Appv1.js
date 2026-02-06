3dfb417762d7209e03d200921b0e2dbca5da0d67
Dayaash G <gdayaash@gmail.com>
2026-02-06 17:11:53 +0530

    Reusable Box using Component Composition Concept

--- src/Appv1.js
+++ src/Appv1.js
@@ -153,6 +153,22 @@ function Movie({ movie }) {
   );
 }
 
+// function WatchedList({ children }) {
+//   const [isOpen2, setIsOpen2] = useState(true);
+
+//   return (
+//     <div className="box">
+//       <button
+//         className="btn-toggle"
+//         onClick={() => setIsOpen2((open) => !open)}
+//       >
+//         {isOpen2 ? "–" : "+"}
+//       </button>
+//       {isOpen2 && children}
+//     </div>
+//   );
+// }
+
 function Watched({ watched }) {
   return (
     <>
