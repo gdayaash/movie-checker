4a02fa586ec3a5181d615ae619b043cdf1f7a29b
Dayaash G <gdayaash@gmail.com>
2026-02-10 15:50:12 +0530

    Star Component

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+const StarBox = {
+  display: "flex",
+  alignItems: "center",
+  gap: 4,
+};
 
-function Star({ index, rating, hover, setHover, setRating }) {
-  const active = index <= (hover || rating);
+export default function Stars() {
+  return (
+    <article role="button" style={StarBox}>
+      <aside>
+        {Array.from({ length: 5 }, (_, i) => (
+          <Star />
+        ))}
+      </aside>
+      <aside>Rating Result</aside>
+    </article>
+  );
+}
+
+function Star() {
+  const starStyle = {
+    width: "20px",
+  };
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="rating-star"
-      fill={active ? "Yellow" : "none"}
+      fill="none"
       viewBox="0 0 24 24"
       stroke="#000"
-      onMouseEnter={() => setHover(index)}
-      onMouseLeave={() => setHover(0)}
-      onClick={() => setRating(index)}
-      style={{ cursor: "pointer" }}
+      style={starStyle}
     >
       <path
         strokeLinecap="round"
@@ -21,24 +36,5 @@ function Star({ index, rating, hover, setHover, setRating }) {
         d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
       />
     </svg>
-  );
-}
-
-export default function Rating() {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
-  return (
-    <div>
-      {Array.from({ length: 10 }, (_, i) => (
-        <Star
-          key={i}
-          index={i + 1}
-          rating={rating}
-          hover={hover}
-          setHover={setHover}
-          setRating={setRating}
-        />
-      ))}
-    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import Stars from "./components/Star";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Stars />
   </React.StrictMode>,
 );
